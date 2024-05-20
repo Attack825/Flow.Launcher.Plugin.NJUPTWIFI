cdeed9d1b27444126cd83b9357fd977e921f662c
Attack825 <2707138687@qq.com>
2024-05-20 23:43:47 +0800

    modified:   plugin/main.py

--- plugin/main.py
+++ plugin/main.py
@@ -4,83 +4,56 @@ from flowlauncher import FlowLauncher
 import sys
 import os
 import urllib.parse
+import json
+from typing import Any, Mapping, Dict
+
+if sys.version_info < (3, 11):
+    from typing_extensions import NotRequired, TypedDict
+else:
+    from typing import NotRequired, TypedDict
+
+
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
 
-def login():
-    account = 'B21090519@njxy'
-    password = 'Xwj20021114.'
-
-    url = 'https://p.njupt.edu.cn:802/eportal/portal/login'
-    params = {
-        'callback': 'dr1003',
-        'login_method': '1',
-        # 'user_account': ',0,B21090519@njxy',
-        'user_account': account,
-        'user_password': password,
-        'wlan_user_ip': '10.136.160.206',
-        'wlan_user_ipv6': '',
-        'wlan_user_mac': '000000000000',
-        'wlan_ac_ip': '',
-        'wlan_ac_name': '',
-        'jsVersion': '4.1.3',
-        'terminal_type': '1',
-        'lang': 'zh-cn',
-        'v': '4028',
-        'lang': 'zh'
-    }
-
-    headers = {
-        'Accept': '*/*',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'
-    }
-
-    response = requests.get(url, params=params, headers=headers)
-
-    # print(response.status_code)
-    # print(response.text)  # 如果需要查看返回的内容
+class JsonRPCRequest(TypedDict):
+    method: str
+    parameters: list
+    settings: NotRequired[dict[Any, Any]]
 
 
-def logout():
-    url = 'https://p.njupt.edu.cn:802/eportal/portal/logout'
-    params = {
-        'callback': 'dr1003',
-        'login_method': '1',
-        'user_account': 'drcom',
-        'user_password': '123',
-        'ac_logout': '1',
-        'register_mode': '1',
-        'wlan_user_ip': '10.136.160.206',
-        'wlan_user_ipv6': '',
-        'wlan_vlan_id': '0',
-        'wlan_user_mac': '000000000000',
-        'wlan_ac_ip': '',
-        'wlan_ac_name': '',
-        'jsVersion': '4.1.3',
-        'v': '7267',
-        'lang': 'zh'
-    }
+class JsonRPCClient:
 
-    headers = {
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5,en-IE;q=0.4',
-        'Cookie': '_ga=GA1.1.148507178.1701333449; _ga_J6LT8812GZ=GS1.1.1701333448.1.1.1701334038.0.0.0',
-        'Referer': 'https://p.njupt.edu.cn/',
-        'Sec-Fetch-Dest': 'script',
-        'Sec-Fetch-Mode': 'no-cors',
-        'Sec-Fetch-Site': 'same-site',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'
-        # 其他可能需要的 header
-    }
+    def send(self, data: Mapping) -> None:
+        json.dump(data, sys.stdout)
 
-    response = requests.get(url, params=params, headers=headers)
+    def recieve(self):
+        try:
+            return json.loads(sys.argv[1])
+        except (IndexError, json.JSONDecodeError):
+            return {'method': 'query', 'parameters': ['']}
 
-    # print(response.status_code)
-    # print(response.text)  # 如果需要查看返回的内容
+
+def settings() -> Dict[str, Any]:
+    """Retrieve the settings from Flow Launcher."""
+    return JsonRPCClient().recieve().get('settings', {})
+
+
+def debug_str(str):
+    return [
+        {
+            "Title": "Hello World Python's Context menu",
+            "SubTitle": str,
+            "IcoPath": "Images/app.png",
+            "JsonRPCAction": {
+                "method": "is_online",
+                "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher.Plugin.HelloWorldPython"]
+            }
+        }
+    ]
 
 
 class MessageDTO:
@@ -100,7 +73,7 @@ class MessageDTO:
             "IcoPath": self.image,
             "jsonRPCAction": {
                 "method": self.funcname,  # 自定义插件的方法
-                "parameters": [self.parameters]  # 自定义插件类的参数
+                "parameters": self.parameters  # 自定义插件类的参数
             }
         }
 
@@ -113,33 +86,38 @@ class HelloWorld(FlowLauncher):
         self.messages.append(message.asFlowMessage())
 
     def query(self,  params: str) -> list:
+        settings = JsonRPCClient().recieve().get("settings", {})
+        account = settings.get("account", None) or None
+        password = settings.get("password", None) or None
         operation_list = [
             {
                 "Title": "login",
                 "SubTitle": "login",
                 "Image": "images/app.png",
-                "FuncName": "cmd_command",
-                "Parameters": "login"
+                "FuncName": "login",
+                "Parameters": [account, password]
             },
             {
                 "Title": "logout",
                 "SubTitle": "logout",
                 "Image": "images/app.png",
-                "FuncName": "cmd_command",
-                "Parameters": "logout"
+                "FuncName": "logout",
+                "Parameters": []
             },
             {
                 "Title": "is_online",
                 "SubTitle": "is_online",
                 "Image": "images/app.png",
-                "FuncName": "open_url",
-                "Parameters": "https://p.njupt.edu.cn:802/eportal/portal/online_list?callback=dr1002&user_account=&user_password=&wlan_user_mac=000000000000&wlan_user_ip=176726222&curr_user_ip=176726222&jsVersion=4.X&v=4182&lang=zh"
+                "FuncName": "is_online",
+                "Parameters": ["https://p.njupt.edu.cn:802/eportal/portal/online_list?callback=dr1002&user_account=&user_password=&wlan_user_mac=000000000000&wlan_user_ip=176726222&curr_user_ip=176726222&jsVersion=4.X&v=4182&lang=zh"]
             }
         ]
+
         for operation in operation_list:
             self.addMessage(MessageDTO(operation))
 
         return self.messages
+        # return debug_str(password)
 
     def open_url(self, url):
         webbrowser.open(url)
@@ -161,11 +139,78 @@ class HelloWorld(FlowLauncher):
         #     }
         # ]
 
-    def cmd_command(self, command):
-        if command == "login":
-            login()
-        elif command == "logout":
-            logout()
+    def login(self, account: str, password: str):
+        account = account
+        password = password
+
+        url = 'https://p.njupt.edu.cn:802/eportal/portal/login'
+        params = {
+            'callback': 'dr1003',
+            'login_method': '1',
+            # 'user_account': ',0,B21090519@njxy',
+            'user_account': account,
+            'user_password': password,
+            'wlan_user_ip': '10.136.160.206',
+            'wlan_user_ipv6': '',
+            'wlan_user_mac': '000000000000',
+            'wlan_ac_ip': '',
+            'wlan_ac_name': '',
+            'jsVersion': '4.1.3',
+            'terminal_type': '1',
+            'lang': 'zh-cn',
+            'v': '4028',
+            'lang': 'zh'
+        }
+
+        headers = {
+            'Accept': '*/*',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'
+        }
+
+        response = requests.get(url, params=params, headers=headers)
+
+        # print(response.status_code)
+        # print(response.text)  # 如果需要查看返回的内容
+
+    def logout(self):
+        url = 'https://p.njupt.edu.cn:802/eportal/portal/logout'
+        params = {
+            'callback': 'dr1003',
+            'login_method': '1',
+            'user_account': 'drcom',
+            'user_password': '123',
+            'ac_logout': '1',
+            'register_mode': '1',
+            'wlan_user_ip': '10.136.160.206',
+            'wlan_user_ipv6': '',
+            'wlan_vlan_id': '0',
+            'wlan_user_mac': '000000000000',
+            'wlan_ac_ip': '',
+            'wlan_ac_name': '',
+            'jsVersion': '4.1.3',
+            'v': '7267',
+            'lang': 'zh'
+        }
+
+        headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5,en-IE;q=0.4',
+            'Cookie': '_ga=GA1.1.148507178.1701333449; _ga_J6LT8812GZ=GS1.1.1701333448.1.1.1701334038.0.0.0',
+            'Referer': 'https://p.njupt.edu.cn/',
+            'Sec-Fetch-Dest': 'script',
+            'Sec-Fetch-Mode': 'no-cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'
+            # 其他可能需要的 header
+        }
+
+        response = requests.get(url, params=params, headers=headers)
+
+        # print(response.status_code)
+        # print(response.text)  # 如果需要查看返回的内容
+    def is_online(self, url):
+        webbrowser.open(url)
 
 
 if __name__ == "__main__":
